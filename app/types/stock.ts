a343102b50e856f21169e4eff6dde05a020d457e
christian <christianbiringanine22@gmail.com>
2025-09-04 08:14:52 +0200

    Initialize the typing

---
+++ app/types/stock.ts
@@ -0,0 +1,10 @@
+// types/stock.ts
+export type StockData = {
+  symbol: string;
+  name: string;
+  price: number;
+  change: number;
+  changePercent: number;
+  volume: number;
+  marketCap: number;
+};
